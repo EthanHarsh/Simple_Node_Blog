abb1d6701ebcf0d3018a68c3d6f1ca72707df1c9
Ethan Harsh <ethan@ethanharsh.com>
2021-03-11 22:31:15 +0000

    init commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRouter = require('./routes/articles');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+
+
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Articles',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Articles2',
+        createdAt: new Date(),
+        description: 'Test description2'
+    }
+    ];
+
+    res.render('articles/index', {articles: articles});
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(5000);
+
